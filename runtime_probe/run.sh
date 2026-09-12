c644c3fd7a35563f2b6ddf2f2e2de917b54fd92f
Timo Reimann <ttr314@googlemail.com>
2026-09-12 02:37:09 +0200

    Clean runtime probe before packaging

--- runtime_probe/run.sh
+++ runtime_probe/run.sh
@@ -5,7 +5,7 @@ IMAGE=devkitpro/devkita64
 PROJECT_DIR=$(cd "$(dirname "$0")" && pwd)
 
 docker run --rm --user "$(id -u):$(id -g)" -e HOME=/tmp \
-    -v "$PROJECT_DIR":/project -w /project "$IMAGE" make package
+    -v "$PROJECT_DIR":/project -w /project "$IMAGE" make clean package
 
 echo "Built $PROJECT_DIR/nx_pctl_runtime_probe.nsp"
 echo "Install bundle: $PROJECT_DIR/nx_pctl_runtime_probe.zip"
